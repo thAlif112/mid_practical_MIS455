850d80e20a2494ffeff6cf051f8eac00487530dd
thAlif112 <thalif2025@gmail.com>
2026-07-07 02:04:49 +0600

    Final commit

---
+++ practical.js
@@ -0,0 +1,14 @@
+// Function for "Discover Our Story" button
+function showAlert() {
+    alert("Welcome to Premium Jersey Shop! We've been serving the finest coffee since 2010. Our passion is bringing you the best jersey experience!");
+}
+
+// Function for "Add to Cart" buttons
+function addToCart(productName) {
+    alert(productName + " has been added to your cart! Thank you for shopping with us.");
+}
+
+// Function for Contact Form submission
+function submitForm() {
+    alert("Thank you for your message! We will get back to you within 2 hours.");
+}
